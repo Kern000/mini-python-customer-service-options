b607b646177b17992befbcc90c9af368ebd66781
unknown <holupkern@gmail.com>
2023-08-31 14:13:45 +0800

    corrected some typos in python comments, minor amendments to readme.md

--- pythonProject.py
+++ pythonProject.py
@@ -1,18 +1,25 @@
+# Packages #
+
+
 import re
 import json
 
 # global variables #
+
+
 customer_list = []
-customer={}
+customer = {}
 
 # Main User Interface #
+
 
 def main():
     user_choice()
 
+
 def display_menu():
     print(
-"""
+        """
 Welcome to ACME Travels System!
 Please choose an option. For example, if you want to list all customers, input '1'
 
@@ -26,26 +33,31 @@ Menu:
 7. Load Data
 8. Exit
 """
-)
+    )
+
+# Utiilty #
+
 
 def return_choice(function):
     return_choice = input('return to main? y/n: ')
-    while return_choice != 'y' and return_choice !='n':
+    while return_choice != 'y' and return_choice != 'n':
         return_choice = input('return to main? y/n: ')
     if return_choice == 'y':
         return main()
     else:
         return function()
 
-# Utiilty #
-def customer_search_initiate_validation(customer_name1):                 #Return Object Datatype
+
+# Return Object Datatype
+def customer_search_initiate_validation(customer_name1):
+
     customer_name = customer_name1
     input_without_spaces = customer_name.replace(" ", "")
     customer_name_validation = input_without_spaces.isalpha()
     while customer_name_validation != True:
         print('Please enter alphabetic letters only')
         customer_name = input('Enter customer\'s name: ')
-        input_without_spaces= customer_name.replace(" ", "")
+        input_without_spaces = customer_name.replace(" ", "")
         customer_name_validation = input_without_spaces.isalpha()
 
     if customer_name_validation == True:
@@ -56,9 +68,11 @@ def customer_search_initiate_validation(customer_name1):                 #Return
         print('customer not found')
         return False
 
-def email_validator(email_address):                                     #Return validated email address
+
+def email_validator(email_address):  # Return validated email address
     email_to_validate = email_address
-    pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'       #r define raw string where \ does not escape
+    # r define raw string where \ does not escape
+    pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
     validated_email = re.match(pattern, email_to_validate)
     while validated_email == None:
         print("Enter valid email Address")
@@ -66,30 +80,36 @@ def email_validator(email_address):                                     #Return 
         validated_email = re.match(pattern, email_to_validate)
     return email_to_validate
 
-def phone_number_validator(new_phone_number):                           #Return validated Phone number
+
+def phone_number_validator(new_phone_number):  # Return validated Phone number
     phone_number_to_validate = new_phone_number
     pattern = r'^[0-9]{0,15}$'
-    validated_phone_number = re.match(pattern, phone_number_to_validate) 
+    validated_phone_number = re.match(pattern, phone_number_to_validate)
     while validated_phone_number == None:
         print("Enter valid phone number")
-        phone_number_to_validate = input('Enter the customer\'s phone number: ')
+        phone_number_to_validate = input(
+            'Enter the customer\'s phone number: ')
         validated_phone_number = re.match(pattern, phone_number_to_validate)
     return int(phone_number_to_validate)
 
-def choice_validator(choice):                           #Return validated Phone number
+
+def choice_validator(choice):  # Return validated choice
     choice_to_validate = choice
     pattern = r'^[0-9]{0,2}$'
-    validated_choice = re.match(pattern, choice_to_validate) 
+    validated_choice = re.match(pattern, choice_to_validate)
     while validated_choice == None:
         print("Enter valid choice")
         choice_to_validate = input('Enter the choice between 1 to 7: ')
-        validated_choice = re.match(pattern, choice_to_validate) 
+        validated_choice = re.match(pattern, choice_to_validate)
     return int(choice_to_validate)
 
+
 # User input carousel
+
+
 def list_all_customers():
     if len(customer_list) > 0:
-       for customer in customer_list:
+        for customer in customer_list:
             print(
                 """
                 'name:' %s
@@ -102,12 +122,13 @@ def list_all_customers():
         print('No customers in record')
     return_choice(list_all_customers)
 
+
 def search_for_customer():
-    
-    customer_name= input('search for customer: ')
-    print('searched for customer name: ',customer_name)
+
+    customer_name = input('search for customer: ')
+    print('searched for customer name: ', customer_name)
     validated_customer = customer_search_initiate_validation(customer_name)
-    
+
     if (validated_customer):
         print(
             """
@@ -121,11 +142,12 @@ def search_for_customer():
         print("no customer found")
     return_choice(search_for_customer)
 
+
 def add_new_customer():
 
     proceed = input('Proceed to enter new customer details? y/n: ')
 
-    while proceed != 'y' and proceed !='n':
+    while proceed != 'y' and proceed != 'n':
         proceed = input('Invalid response. Enter new customer details? y/n: ')
 
     if proceed == 'n':
@@ -143,12 +165,15 @@ def add_new_customer():
                 print('name already exists')
                 new_customer_name = input('Enter the new customer\'s name: ')
 
-        validated_new_customer_name = new_customer_name.replace(" ","").isalpha()
-        
+        validated_new_customer_name = new_customer_name.replace(
+            " ", "").isalpha()
+
         while validated_new_customer_name != True:
-            new_customer_name = input('Enter the new customer\'s name (alphabets only): ')
-            validated_new_customer_name = new_customer_name.replace(" ","").isalpha()
-        
+            new_customer_name = input(
+                'Enter the new customer\'s name (alphabets only): ')
+            validated_new_customer_name = new_customer_name.replace(
+                " ", "").isalpha()
+
         new_email_address = input('Enter the customer\'s email address: ')
         validated_email_address = email_validator(new_email_address)
 
@@ -161,18 +186,18 @@ def add_new_customer():
             'phone_number': validated_phone_number
         }
 
-        print (
-                """
+        print(
+            """
                 Entered particulars:
                 name: %s
                 email: %s
                 phone_number: %d
                 """
-                %(new_customer_name, validated_email_address, validated_phone_number)
+            % (new_customer_name, validated_email_address, validated_phone_number)
         )
 
         confirmation = input('Confirm customer details? y/n: ')
-        while confirmation != 'y' and confirmation !='n':
+        while confirmation != 'y' and confirmation != 'n':
             print('enter valid choice')
             confirmation = input('Confirm customer details? y/n: ')
         if confirmation == 'y':
@@ -183,81 +208,91 @@ def add_new_customer():
         elif confirmation == 'n':
             return_choice(add_new_customer)
 
+
 def update_existing_customer():
 
-        confirm = 'n'
-        customer_to_search = input('search for customer by name: ')
-        print('Customer searched: ', customer_to_search)
+    confirm = 'n'
+    customer_to_search = input('search for customer by name: ')
+    print('Customer searched: ', customer_to_search)
 
-        found_customer = customer_search_initiate_validation(customer_to_search)  #return customer or print no customer found
+    found_customer = customer_search_initiate_validation(
+        customer_to_search)  # return customer or print no customer found
 
-        if found_customer:
-            print (found_customer)
-            confirm = input('Update this customer? y/n: ')
+    if found_customer:
+        print(found_customer)
+        confirm = input('Update this customer? y/n: ')
 
-            while confirm != 'y' and confirm !='n':
-                print('Enter valid confirmation y/n')
-                confirm = input('Invalid response. Update this customer? y/n: ')
-                
-            if confirm == 'y':
-                new_customer_name = input('Enter updated customer\'s name: ')
-                validated_new_customer_name = new_customer_name.replace(" ","").isalpha()
-                
-                while validated_new_customer_name != True:
-                    new_customer_name = input('Enter updated customer\'s name (alphabets only): ')
-                    validated_new_customer_name = new_customer_name.replace(" ","").isalpha()
-                    
-                new_email_address = input('Enter updated customer\'s email address: ')
-                validated_email_address = email_validator(new_email_address)
+        while confirm != 'y' and confirm != 'n':
+            print('Enter valid confirmation y/n')
+            confirm = input('Invalid response. Update this customer? y/n: ')
 
-                new_phone_number = input('Enter updated customer\'s phone number: ')
-                validated_phone_number = phone_number_validator(new_phone_number)
+        if confirm == 'y':
+            new_customer_name = input('Enter updated customer\'s name: ')
+            validated_new_customer_name = new_customer_name.replace(
+                " ", "").isalpha()
 
-                customer_entry = {
-                    'name': new_customer_name.strip(),
-                    'email': validated_email_address.strip(),
-                    'phone_number': validated_phone_number
-                }
+            while validated_new_customer_name != True:
+                new_customer_name = input(
+                    'Enter updated customer\'s name (alphabets only): ')
+                validated_new_customer_name = new_customer_name.replace(
+                    " ", "").isalpha()
 
-                found_customer_index = customer_list.index(found_customer)
-                customer_list[found_customer_index] = customer_entry
-                print('Customer updated:')
-                print(customer_entry)
+            new_email_address = input(
+                'Enter updated customer\'s email address: ')
+            validated_email_address = email_validator(new_email_address)
 
-                return_choice(update_existing_customer)                
-            else:
-                return_choice(update_existing_customer)
+            new_phone_number = input(
+                'Enter updated customer\'s phone number: ')
+            validated_phone_number = phone_number_validator(new_phone_number)
+
+            customer_entry = {
+                'name': new_customer_name.strip(),
+                'email': validated_email_address.strip(),
+                'phone_number': validated_phone_number
+            }
+
+            found_customer_index = customer_list.index(found_customer)
+            customer_list[found_customer_index] = customer_entry
+            print('Customer updated:')
+            print(customer_entry)
+
+            return_choice(update_existing_customer)
         else:
             return_choice(update_existing_customer)
+    else:
+        return_choice(update_existing_customer)
 
 
 def delete_customer():
 
-        confirm = 'n'
-        customer_to_search = input('search for customer to delete by name: ')
-        print('Customer searched: ', customer_to_search)
+    confirm = 'n'
+    customer_to_search = input('search for customer to delete by name: ')
+    print('Customer searched: ', customer_to_search)
 
-        found_customer = customer_search_initiate_validation(customer_to_search)  #return customer or print no customer found
-        if found_customer:
-            print (found_customer)
-            confirm = input('delete this single customer entry? y/n: ')
+    found_customer = customer_search_initiate_validation(
+        customer_to_search)  # return customer or print no customer found
+    if found_customer:
+        print(found_customer)
+        confirm = input('delete this single customer entry? y/n: ')
 
-            while confirm != 'y' and confirm !='n':
-                print('Enter valid confirmation y/n')
-                confirm = input('Invalid response. Delete this single customer entry? y/n: ')
-                
-            if confirm == 'y':
-            
-                found_customer_index = customer_list.index(found_customer)
-                del customer_list[found_customer_index]
-                print('Customer entry deleted')
+        while confirm != 'y' and confirm != 'n':
+            print('Enter valid confirmation y/n')
+            confirm = input(
+                'Invalid response. Delete this single customer entry? y/n: ')
 
-                return_choice(delete_customer)
+        if confirm == 'y':
 
-            else:
-                    return_choice(delete_customer)
+            found_customer_index = customer_list.index(found_customer)
+            del customer_list[found_customer_index]
+            print('Customer entry deleted')
+
+            return_choice(delete_customer)
+
         else:
             return_choice(delete_customer)
+    else:
+        return_choice(delete_customer)
+
 
 def save_and_export_data():
 
@@ -270,6 +305,7 @@ def save_and_export_data():
         print('no customer exist')
         return_choice(save_and_export_data)
 
+
 def load_data():
 
     global customer_list
@@ -279,23 +315,26 @@ def load_data():
         choice = input("invalid input. Proceed to load data? y/n: ")
 
     if choice == 'y':
-        file_path = input("Enter file name with extension in current folder, or if not, it\'s full filepath: ")
+        file_path = input(
+            "Enter file name with extension in current folder, or if not, it\'s full filepath: ")
         if file_path[-5:] != ".json":
             print('unsupported file format')
             return_choice(load_data)
         else:
-            data_handling_method = input("Choose data handling method. Key in 'append' or 'overwrite': ")
+            data_handling_method = input(
+                "Choose data handling method. Key in 'append' or 'overwrite': ")
 
             while data_handling_method != 'append' and data_handling_method != 'overwrite':
-                data_handling_method = input("Choose a valid data handling method. Key in 'append' or 'overwrite': ")
+                data_handling_method = input(
+                    "Choose a valid data handling method. Key in 'append' or 'overwrite': ")
 
             try:
-                if data_handling_method == 'append':                
+                if data_handling_method == 'append':
                     with open(file_path, "r") as file:
                         data = json.load(file)
                     for customer in data:
                         customer_list.append(customer)
-                    print ('Customer List =>', customer_list)
+                    print('Customer List =>', customer_list)
                     return_choice(load_data)
 
                 elif data_handling_method == 'overwrite':
@@ -303,14 +342,15 @@ def load_data():
 
                     overwrite_decision = input('confirm overwrite? y/n: ')
 
-                    while overwrite_decision !="y" and overwrite_decision !="n":
-                        overwrite_decision = input('Enter valid command. Confirm overwrite? y/n: ')
+                    while overwrite_decision != "y" and overwrite_decision != "n":
+                        overwrite_decision = input(
+                            'Enter valid command. Confirm overwrite? y/n: ')
 
                     if overwrite_decision == 'y':
                         with open(file_path, "r") as file:
                             data = json.load(file)
                             customer_list = data
-                        print ('Retrieved Customer List =>', customer_list)
+                        print('Retrieved Customer List =>', customer_list)
                         return_choice(load_data)
 
                     else:
@@ -321,18 +361,19 @@ def load_data():
     else:
         return_choice(load_data)
 
+
 def user_choice():
 
     choice = 0
-    while choice not in range(1,9):
+    while choice not in range(1, 9):
         display_menu()
         choice = input("Please enter a choice 1-8: ")
         choice = choice_validator(choice)
-           
+
         if choice == 1:
             print('Chosen: list customers')
             list_all_customers()
-        
+
         elif choice == 2:
             print('Chosen: search for customer')
             search_for_customer()
@@ -352,22 +393,23 @@ def user_choice():
         elif choice == 6:
             print('Chosen: Save customer')
             save_and_export_data()
-        
+
         elif choice == 7:
             print('Chosen: load data from file')
             load_data()
 
         elif choice == 8:
-            exit_choice = input("Have you saved and exported the file? unsaved data will be lost y/n: ")
+            exit_choice = input(
+                "Have you saved and exported the file? unsaved data will be lost y/n: ")
             while exit_choice != 'y' and exit_choice != 'n':
-                choice = input("Invalid entry. Have you save and exported the file? unsaved data will be lost y/n: ")
-            if exit_choice == 'n':   
+                choice = input(
+                    "Invalid entry. Have you save and exported the file? unsaved data will be lost y/n: ")
+            if exit_choice == 'n':
                 user_choice()
-            else:          
+            else:
                 print("Thank you for visiting. We hope to see you again")
                 break
 
+
 if __name__ == "__main__":
     main()
-
-
--- readme.md
+++ readme.md
@@ -7,35 +7,53 @@ This is project one for BELLS SG basic python course for business automation.
 This is a python application that allows the admin user to navigate between methods to create, read, update, delete and export the customers' collected information.
 
 Functional Requirements
+
 FR1.1: The system allows users to create a new customer entry.
+
 FR1.1.1: Each new customer entry must be filled in and cannot be left blank, and the input must match the required structure.
+
 FR1.2: The system allows users to list the existing customers entries.
+
 FR1.3: The system allows users to search for a customer entry.
+
 FR1.4: The system allows users to update a customer entry.
+
 FR1.4.1: Each new customer entry must be filled in and cannot be left blank, and the input must match the required structure.
+
 FR1.5: The system allows users to delete a customer entry.
+
 FR1.6: The system allows users to save and export the list of customer entries into a file on local storage.
+
 FR1.7: The system allows user to load a list from file into the application to add on to existing data or to modify it.
+
 FR1.7.1: The file type must be .json and the file must exist in the same folder where program is runned.
 
 Non-Functional Requirements
+
 NFR2.1: Security - user input is validated based on datatype and size. This prevents cross-site scripting and poor data entry.
 
 Task Attributes
-NFR3.1: Accessibility - users are given the choice to confirm their input or to reinput when there are mistakes in data entry.
+
+NFR3.1: Accessibility - users are given the choice to confirm their input or to reinput them when there are mistakes in data entry.
+
 NFR3.2: Accessibility - users only exit a function when they choose to, allowing them to continue working on a similar type of task repeatedly.
+
 NFR3.3: Accessibility - users error allows the application to continue operating and users can continue to navigate within the application seamlessly.
-NFR3.4: Error catching - errors in uploading files like uploading unsupported file types are caught and prevents the application from break.
+
+NFR3.4: Error catching - errors in uploading files like uploading unsupported file types are caught and prevents the application from breaking.
 
 ===== Design =====
 
-Python is chosen due to the ease of input validation. Python's "re" package allow easy use of regex matching to perform user input validation, the prevalent operation in this application.
+Python is chosen due to the ease of input validation. Python's "re" package allow easy use of regex matching to perform user input validation, which is the prevalent operation in this application.
+
 Procedural programming is employed, breaking down the application's operations into small functions to perform tasks in bitesize, efficient manners. This can be implemented because users may not need to create, update, and delete within a single workflow.
+
 This allows the user and the application to run only the necessary functions and operations for their intentions and purpose.
 
 Additionally, functional programming is also employed.
 
 Functions that are often utilized across different operations were refactored into reuseable functions, and in some instances, higher order functions.
+
 Specifically, the return_choice function accepts a core operation function as an argument, allowing restarting or exiting of an operation when a process needs to be restarted or cut short as it is more efficient to do so.
 
 PEP8 is adhered to. Each line of code is kept short and concise. Functions and variables are written in descriptive manners.
@@ -43,54 +61,91 @@ In other words, functions are verbs that are descriptive of an action. Variables
 
 ====== UI/UX ======
 
-The application can be seamlessly navigated with the typing of short y/n letters or single numeric number.
+The application can be seamlessly navigated with the typing of short y/n letters or a single numeric number.
 Important confirmations, like the overwriting or appending of data during uploading operations, require the user to type 'append' or 'overwrite' to confirm their intentions.
 
 ====== Testing ======
 
-Test Case 1
-Name of test: customer add and search
+Unit Test One: Add new customer and Search for added customer
 
-== Success Case ==
+-- Outcome 1 --
 
-Press 3 to Add customer entry:
-key in:
-Alex Goh
-alexgoh@gmail.com
-99090238
+Function Tested: add_new_customer
 
-Expected:
-Confirmation printed on terminal. It should display:
-Alex Goh
-alexgoh@gmail.com
-99090238
-press y to save customer details.
+Name of Test: Add new customer and Search for added customer
 
-On main terminal, press 2. to search for Alex Goh (with matching case) and it should display the same.
+Input:
 
-Test Case 2
-Name of Test: Data entry validation
+Press 3 to Add customer entry
 
-== Failure case for testing validation ==
+Input: Enter 'Alex Goh' for name
+
+Input: Enter 'alexgoh@gmail.com' for email
+
+Input: 99090238 for phone_number
+
+Expected Output:
+
+Input data is printed on terminal:
+
+Entered Particulars:
+
+name: Alex Goh
+
+email: alexgoh@gmail.com
+
+phone_number: 99090238
+
+press y to confirm customer details.
+
+On main terminal, press 2. Input: Alex Goh (with matching case) and it should display the same.
+
+-- Outcome 2 --
+
+Function Tested: add_new_customer
+
+Name of Test: Add new customer and Search for added customer
+
+Input:
+
+Press 3 to Add customer entry
 
 Try to add the following customer details:
-Alex Goh 5
-alexgod.com
+'Alex Goh 5'
+'alexgod.com'
 +6590909090
 
-Expected Output: It should prompt for reentry of the data, guiding user on the correct information to enter.
+Expected Output: prompt user for reentry of the data, guiding user on the correct information to enter.
 
-Test Case 3
-name of Test: Testing file load
+=========
 
-== Testing for the loading of wrong data type ==
+Unit Test Two: Testing file loading
 
-Enter meow.csv (an unsupported file format) -> Expected: it will inform unsupported file format
-Enter meow.json (a non-existent file name) -> Expected: After choosing append or overwrite, it will detect if file exist. Since it does not, it will inform file not found.
+-- Outcome 1 --
+
+Function Tested: load_data
+
+Name of Test: loading data of required file type
+
+Input:
+Enter meow.csv (an unsupported file format)
+
+Expected Output: console prints 'unsupported file format' and prompts reinput or return main page.
+
+-- Outcome 2 --
+
+Function Tested: load_data
+
+Name of Test: loading data of required file type
+
+Input: Enter meow.json (a non-existent file name)
+
+Expected Output: console will print 'file not found' and prompt reinput or return to main.
 
 ====== Two Possible Enhancements ======
 
 1. Login system can be implemented to allow authentication and authorization.
+
 2. Indexing can be implemented for search to allow high performance when large datasets are expected.
 
 Thank you for reading.
